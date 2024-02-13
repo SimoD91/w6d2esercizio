aba061f646de4bedc496a4b9d0ff50069d3a56f0
SimoD91 <simone0091@hotmail.com>
2024-02-13 16:05:08 +0100

    Initial commit

---
+++ src/test/java/it/epicode/w6d2esercizio/W6d2esercizioApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.w6d2esercizio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W6d2esercizioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
